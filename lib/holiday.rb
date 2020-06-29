309e6ca62e0845599b00eaac6e9316c049a6cdef
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-29 21:36:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do|season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday,supplies|
-      
+      supplies = supplies.join("'")
 
 end
 
